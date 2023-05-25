2f957a22ea2a9f4dd0b7112ac55dcebb8fb5041c
Peter <schmidt.multimedia@gmail.com>
2023-05-25 15:26:19 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,9 @@ public class App {
     
     public static void main(String[] args) {
      
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello("Roxana"));
        
        
 
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Parent extends GrandParent{
+  
+    public String sayHello() {
+        return "Hello from parent!";
+    }
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+
+}
